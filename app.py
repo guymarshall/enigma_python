353d72d4f7d545897c51b7cff6b8b75ebd33d557
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2022-12-12 12:29:46 +0000

    Change rotor_1 and rotor_2 backwards steps to backwards for rotor in rotors

--- app.py
+++ app.py
@@ -6,15 +6,13 @@ For each character that IS NOT " " OR IS NOT punctuation OR IS NOT a number
     if character is to be swapped, swap character
     set new value to character
 
-    foreach rotor in rotors:
+    for rotor in rotors:
         pass character through rotor
         alphabet[0] goes to rotor[0] etc
         set new value to character
         move rotor values by 1 position
 
-    go backwards to rotor_2 and repeat those steps
-
-    go backwards to rotor_1 and repeat those steps
+    go backwards for rotor in rotors and repeat those steps
 
     go backwards to switchboard and repeat those steps
 
