6505dbe541119ebacb53561ef7a48df07b8cd4ac
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2022-12-14 12:16:30 +0000

    Rename rotation_count to rotations

--- rotor.py
+++ rotor.py
@@ -1,10 +1,10 @@
 class Rotor:
     def __init__(self, letters):
         self.letters = letters
-        rotate_count = 0
+        rotations = 0
         alphabet_lower = list("abcdefghijklmnopqrstuvwxyz")
         alphabet_upper = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
     def rotate(self):
         self.letters = self.letters[1:] + self.letters[:1]
-        self.rotate_count += 1
+        self.rotations = self.rotations + 1
