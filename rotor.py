6707749734d90f7dc79f6ef0f655f3ba4c3ff683
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2022-12-14 12:11:17 +0000

    Add alphabet_lower and alphabet_upper lists to rotor.py

--- rotor.py
+++ rotor.py
@@ -2,6 +2,8 @@ class Rotor:
     def __init__(self, letters):
         self.letters = letters
         rotate_count = 0
+        alphabet_lower = list("abcdefghijklmnopqrstuvwxyz")
+        alphabet_upper = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
     def rotate(self, letters):
         self.letters = letters[1:] + letters[:1]
