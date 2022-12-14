8b503473e486c2c9c6723d788d563933538ffd72
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2022-12-14 12:16:52 +0000

    Add "self." to remaining member fields that were missing it

--- rotor.py
+++ rotor.py
@@ -1,9 +1,9 @@
 class Rotor:
     def __init__(self, letters):
         self.letters = letters
-        rotations = 0
-        alphabet_lower = list("abcdefghijklmnopqrstuvwxyz")
-        alphabet_upper = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+        self.rotations = 0
+        self.alphabet_lower = list("abcdefghijklmnopqrstuvwxyz")
+        self.alphabet_upper = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
     def rotate(self):
         self.letters = self.letters[1:] + self.letters[:1]
